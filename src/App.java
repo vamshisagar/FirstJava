498a09167657228ed5f866475495579f5767093e
Vamshi Molla <vamshimolla@Vamshis-MacBook-Air.local>
2021-11-28 19:46:23 +0530

    AddedArraylist

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        for(Integer i: al)
+        {
+            System.out.println(i);
+        }
+
     }
 }
